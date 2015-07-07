6ec4de8c0e5729b4ebcc0733410d38ce2fbde8c8
attila3453 <attila.geiszt@gmail.com>
2015-07-07 17:43:06 +0300

    Solve Problem 6 and 7

--- Probleme/Problem6.cs
+++ Probleme/Problem6.cs
@@ -6,11 +6,63 @@ using System.Threading.Tasks;
 
 namespace Probleme
 {
+    // Converting Arabic to Roman: We would like to be able to convert Arabic numbers into their Roman numeral equivalents. 
+    // We just need some kind of program that can accept a numeric input and output the Roman numeral for the input number.
     class Problem6
     {
         public static void solve()
         {
-            Console.WriteLine("problema 1");
+            Console.WriteLine("Problema 6");
+
+            Console.WriteLine("Enter Arabic number:");
+
+            var line = Console.ReadLine();
+
+            if(string.IsNullOrEmpty(line))
+            {
+                Console.WriteLine("Null string entered.");
+                return;
+            }
+
+            foreach(var ch in line)
+                if(!char.IsDigit(ch))
+                {
+                    Console.WriteLine("Input must be formed exclusively by digits.");
+                    return;
+                }
+
+            var number = int.Parse(line);
+
+            var dictRoman = new Dictionary<int, string>()
+            {
+                {1000, "M"}, 
+                {900, "CM"}, 
+                {500, "D"}, 
+                {400, "CD"}, 
+                {100, "C"}, 
+                {90, "XC"},  
+                {50, "L"}, 
+                {40, "XL"}, 
+                {10, "X"}, 
+                {9, "IX"}, 
+                {5, "V"},
+                {4, "IV"}, 
+                {1, "I"}
+            };
+
+            Console.WriteLine("Roman representation:");
+
+            while(number > 0)
+            {
+                foreach(var pair in dictRoman)
+                    while(number >= pair.Key)
+                    {
+                        number -= pair.Key;
+                        Console.Write(pair.Value);
+                    }
+            }
+
+            Console.WriteLine();
         }
     }
 }
--- Probleme/Problem7.cs
+++ Probleme/Problem7.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Drawing;
+using System.IO;
 
 namespace Probleme
 {
@@ -10,7 +12,35 @@ namespace Probleme
     {
         public static void solve()
         {
-            Console.WriteLine("problema 1");
+            Console.WriteLine("Problema 7");
+
+            Console.WriteLine("Enter image path:");
+            var path = Console.ReadLine();
+            Console.WriteLine("Enter console width:");
+            var wConsole = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter console height:");
+            var hConsole = int.Parse(Console.ReadLine());
+
+            var bitmap = (Bitmap)Image.FromFile(path);
+            var bitmapResized = new Bitmap(wConsole, hConsole);
+            var g = Graphics.FromImage((Image)bitmapResized);
+            g.DrawImage(bitmap, 0, 0, bitmapResized.Width, bitmapResized.Height);
+
+            for (var y = 0; y < bitmapResized.Height; y++)
+            {
+                for (var x = 0; x < bitmapResized.Width; x++)
+                {
+                    var pixel = bitmapResized.GetPixel(x, y);
+
+                    var depth = (pixel.R + pixel.G + pixel.B + pixel.A) / 255.0 / 4.0 * 100.0;
+
+                    char ch = (char)('.' + (int)depth);
+
+                    Console.Write(ch);
+                }
+
+                Console.WriteLine();
+            }
         }
     }
 }
